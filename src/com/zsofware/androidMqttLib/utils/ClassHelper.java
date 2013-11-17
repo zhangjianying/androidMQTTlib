26a20c9c907b1d1133805913c1bc14c96fc10cc9
zhangjianying <21961252@qq.com>
2013-11-17 09:46:45 +0800

    a

--- src/com/zsofware/androidMqttLib/utils/ClassHelper.java
+++ src/com/zsofware/androidMqttLib/utils/ClassHelper.java
@@ -1,11 +1,27 @@
 package com.zsofware.androidMqttLib.utils;
 
+import java.util.HashMap;
+
 public class ClassHelper {
+
+	HashMap<String, Object>	cacheMap	= new HashMap<String, Object>();
 
 	public static Object newInstance(String clz) throws ClassNotFoundException,
 			InstantiationException, IllegalAccessException {
-		Class<?> forName = Class.forName(clz);
-		return forName.newInstance();
+		
+		return newInstance(clz, false);
+	}
 
+	public static Object newInstance(String clz, boolean useCache)
+			throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException {
+		Object retVal = null;
+		if (useCache) {
+		} else {
+			Class<?> forName = Class.forName(clz);
+			retVal = forName.newInstance();
+		}
+		
+		return retVal;
 	}
 }
